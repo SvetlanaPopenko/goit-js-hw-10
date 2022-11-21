dd9ef41bec439e2770b9b79510e79be4e635734d
SvetlanaPopenko <spopenko02@gmail.com>
2022-11-22 01:29:35 +0200

    Update refs.js

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,5 +1,5 @@
-const refs{
-form : document.querySelector('#search-box');
-countryList : document.querySelector('.country-list');
-countryInfo : document.querySelector('.country-info');
-}
+export const refs = {
+  form: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
+};
