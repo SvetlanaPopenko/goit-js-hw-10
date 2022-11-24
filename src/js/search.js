20d73fe30ee1238a66f46a4c783e1405b949a7d1
SvetlanaPopenko <spopenko02@gmail.com>
2022-11-24 23:00:10 +0200

    Update search.js

--- src/js/search.js
+++ src/js/search.js
@@ -1,2 +1,53 @@
 import { fetchCountries } from './fetchCountries';
 import { refs } from './refs';
+import { notifyFailure,notifyInfo } from './notify';
+import { addCountryList, addCountryInfo } from './addCountry';
+
+export function onSearch(evt) {
+    evt.preventDefault();
+
+    const searchQuery = evt.target.value.trim();
+    refs.countryList.innerHTML = '';
+    refs.countryInfo.innerHTML = '';
+
+    if (!searchQuery) {
+        return
+    }
+    fetchCountries(searchQuery)
+        .then(country => {
+            if (country.length > 10) {
+                return notifyInfo;
+            }
+            if (country.length > 1 && country.length < 10) {
+                refs.countryList.innerHTML = addCountryList(country);
+            } else {
+                refs.countryInfo.innerHTML = addCountryInfo(country);
+            }
+        })
+        .catch(error => notifyFailure());
+}
+function onSearch(evt) {
+    evt.preventDefault();
+
+    const searchQuery = evt.target.value.trim();
+    refs.countryList.innerHTML = '';
+    refs.countryInfo.innerHTML = '';
+
+    if (!searchQuery) {
+        return
+    }
+    fetchCountries(searchQuery)
+        .then(country => {
+            if (country.length > 10) {
+                return notifyInfo();
+            }
+            if (country.length > 1 && country.length < 10) {
+                refs.countryList.innerHTML = addCountryList(country);
+            } else {
+                refs.countryInfo.innerHTML = addCountryInfo(country);
+            }
+        })
+        .catch(error => notifyFailure());
+}
+
+
