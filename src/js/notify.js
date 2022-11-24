6e569cb4987714a7570a024a676850769738e3cb
SvetlanaPopenko <spopenko02@gmail.com>
2022-11-24 23:00:07 +0200

    Update notify.js

--- src/js/notify.js
+++ src/js/notify.js
@@ -1,1 +1,13 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+export const notifyFailure = () => {
+  return Notify.failure('Oops, there is no country with that name', {
+    timeout: 1000,
+  });
+};
+export const notifyInfo = () => {
+  return Notify.info(
+    'Too many matches found. Please enter a more specific name.',
+    { timeout: 1000 }
+  );
+};
