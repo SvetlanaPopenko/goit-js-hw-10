5bbd2ac5b1d1dbe32502c82c82df4c6dd6cdf5ed
SvetlanaPopenko <spopenko02@gmail.com>
2022-11-24 23:00:02 +0200

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,65 +1,14 @@
+const BASE_URL = 'https://restcountries.com/v3.1/name/';
 
-const BASE_URL = 'https://restcountries.com/v3.1/name/'
-
-import {refs} from './refs'
-
-refs.form.addEventListener('submit', onSearch);
-
-function onSearch(evt) {
-    evt.preventDefault();
-
-    const searchQuery = evt.target.value.trim();
-    refs.countryList.innerHTML = '';
-    refs.countryInfo.innerHTML = '';
-
-    if (!searchQuery) {
-        return
-    }
-    fetchCountries(searchQuery)
-        .then(country => {
-            if (country.length > 10) {
-                return notifyInfo;
-            }
-            if (country.length > 1 && country.length < 10) {
-                refs.countryList.innerHTML = addCountryList(country);
-            } else {
-                refs.countryInfo.innerHTML = addCountryInfo(country);
-            }
-        })
-        .catch(error => notifyFailure());
-}
-const notifyFailure = () => {
-    return Notify.failure('Oops, there is no country with that name', { time: 1000 }
-    )
-};
-const notifyInfo = () => {
-    return Notify.info(
-        'Too many matches found. Please enter a more specific name.', { time: 1000 }
-    )
-}
-        
 export function fetchCountries(searchValue) {
-    return fetch(`${BASE_URL}${searchValue}?fields=name.official,capital,population,flags,languages`)
+    return fetch(`${BASE_URL}${searchValue}?fields=name,capital,population,flags,languages`)
         .then(resp => {
             console.log(resp);
             if (!resp.ok) {
-                throw new Error(resp.statusText)
+                throw new Error(resp.statusText);
             }
-            return resp.json()
+            return resp.json();
         })
-        .catch(err => console.error(err));
+        .catch(err => console.error(error));
 } 
 
-
-function addCountryList(arr) {
-    return arr.map(country => {
-        `<li class="country__item"><img class "country__img" src="${country.flag.svg}" alt="${country.name}" width = "100"/>
-    <h2>${country.name.official}</h2> </li>`
-    }).join('');
-}
-
-function addCountryInfo(arr) {
-    return arr.map(country => {
-        ``
-    })
-}
