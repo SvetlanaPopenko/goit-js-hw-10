249895b812084bef8ee484898eaa1b9185287c40
SvetlanaPopenko <spopenko02@gmail.com>
2022-11-24 23:18:01 +0200

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,14 +1,14 @@
 const BASE_URL = 'https://restcountries.com/v3.1/name/';
 
 export function fetchCountries(searchValue) {
-    return fetch(`${BASE_URL}${searchValue}?fields=name,capital,population,flags,languages`)
-        .then(resp => {
-            console.log(resp);
-            if (!resp.ok) {
-                throw new Error(resp.statusText);
-            }
-            return resp.json();
-        })
-        .catch(err => console.error(error));
-} 
-
+  return fetch(
+    `${BASE_URL}${searchValue}?fields=name,capital,population,flags,languages`
+  )
+    .then(resp => {
+      if (!resp.ok) {
+        throw new Error(resp.statusText);
+      }
+      return resp.json();
+    })
+    .catch(error => console.error(error));
+}
