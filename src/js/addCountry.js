75a457510f5369611289a4ca6b1ebeaeac089990
SvetlanaPopenko <spopenko02@gmail.com>
2022-11-26 12:45:59 +0200

    Update addCountry.js

--- src/js/addCountry.js
+++ src/js/addCountry.js
@@ -12,10 +12,10 @@ function addCountryList(arr) {
 function addCountryInfo(arr) {
   return arr
     .map(country => {
-        return `<div>
+        return `<div class="country">
       <img src="${country.flags.svg}" alt="${country.name}" width= "200">
       <h2 class="country__title">${country.name.official}</h2>
-      <p class="country_desc">Capital:${country.capital}</p>
+      <p class="country__desc">Capital:${country.capital}</p>
       <p class="country__desc">Population:${country.population}</p>
       <p class="country__desc">Languages:${Object.values(country.languages)}</p>
     </div>`;
